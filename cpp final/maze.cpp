d2b1612d9afed4aa9cac874a128cd613593361c5
easeghehey <emanuelaseghey9@gmail.com>
2022-11-29 21:37:30 -0500

    whole thing worksgit add .

--- cpp final/maze.cpp
+++ cpp final/maze.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <map>
 #include <tuple>
+#include <queue>
+
+int totalsize = 0;
 
 struct Node {
     Node* North;
@@ -15,6 +18,16 @@ struct Node {
     Node* West;
     Node* Up;
     Node* Down;
+    int id;
+
+    Node() {
+        North = nullptr;
+        South = nullptr;
+        East = nullptr;
+        West = nullptr;
+        Up = nullptr;
+        Down = nullptr;
+    }
 };
 
 std::string getDirections(const std::string& binaryString){    
@@ -66,6 +79,29 @@ void addToTuple(std::ifstream& file, std::tuple<int, int, int>& tup){
     file >> std::get<2>(tup);
 }
 
+void helper(Node v, std::vector<bool>& visited, std::vector<std::string> path, std::string direction, std::queue<std::pair<Node, std::vector<std::string>>>& q){
+
+    if (visited[v.id] == false) {
+        visited[v.id] = true;
+        path.push_back(direction);
+        std::pair<Node, std::vector<std::string>> addToQueue(v, path);
+        q.push(addToQueue);
+    }
+}
+
+std::string bfs(Node start, Node end){
+    // create visit
+    std::vector<bool> visited;
+    visited.resize(totalsize, false);
+    std::queue<std::tuple<Node, std::vector<std::string>>> queue;
+    visited[start.id] = true;
+
+    while(!queue.empty()){
+        std::tuple<Node, std::vector<std::string>> nodeTuple = queue.front();
+        queue.pop();
+    }
+}
+
 int main(){
     
     std::ifstream file("/Users/emanuelaseghehey/Development/Itsy-Bitsy-Spider-algo/textfiles/tiny-maze.txt");
@@ -100,6 +136,8 @@ int main(){
                 file >> temp;
                 current_row.push_back(getDirections(temp));
                 Node cur;
+                cur.id = totalsize;
+                totalsize += 1;
                 buildNodes.push_back(cur);
             }
             buildNodeGraph.push_back(buildNodes);
@@ -139,8 +177,75 @@ int main(){
         }
     }
 
+    // test
+    // for (int i = 0; i < binaryGraph.size(); i++){
+    //     for (int j = 0; j < binaryGraph[i].size(); j++){
+    //         for (int k = 0; k < binaryGraph[i][j].size(); k++){
+    //             std::cout << graph[i][j][k].id << ' ';
+    //         }
+    //         std::cout << std::endl;
+    //     }
+    //     std::cout << std::endl;
+    // }
+
     // std::cout << graph[0][0][0].Up << std::endl;
     // std::cout << &graph[1][0][0] << std::endl;
+
+    std::vector<bool> visited;
+    visited.resize(totalsize, false);
+    std::queue<std::pair<Node, std::vector<std::string>>> queue;
+    std::vector<std::string> start_path;
+
+    std::pair<Node, std::vector<std::string>> p(graph[0][0][0], start_path);
+    queue.push(p);
+    visited[graph[0][0][0].id] = true;
+
+    while(!queue.empty()){
+
+        std::pair<Node, std::vector<std::string>> current = queue.front();
+        queue.pop();
+
+        Node cur = current.first;
+        std::vector<std::string> path = current.second;
+
+        // test
+        // for (int i = 0; i < path.size(); i ++){
+        //         std::cout << path[i] << ' ';
+        // }
+        // std::cout << std::endl;
+
+        if (cur.id == graph[2][2][2].id){
+            for (int i = 0; i < path.size(); i ++){
+                std::cout << path[i] << ' ';
+            }
+            // break;
+        }
+
+        if (cur.North != nullptr){
+            helper(*cur.North, visited, path, "N", queue);
+        }
+
+        if (cur.South != nullptr){
+            // come back
+            helper(*cur.South, visited, path, "S", queue);
+        }
+
+        if (cur.East != nullptr){
+            helper(*cur.East, visited, path, "E", queue);
+        }
+
+        if (cur.West != nullptr){
+            helper(*cur.West, visited, path, "W", queue);
+        }
+
+        if (cur.Up != nullptr){
+            helper(*cur.Up, visited, path, "U", queue);
+        }
+
+        if (cur.Down != nullptr){
+            helper(*cur.Down, visited, path, "D", queue);
+        }
+    }
 
     return 0;
 }
