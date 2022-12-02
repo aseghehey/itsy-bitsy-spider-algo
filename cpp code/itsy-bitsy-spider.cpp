a327d59e41204c720a39717e9ad1edc484cb5358
easeghehey <emanuelaseghey9@gmail.com>
2022-12-01 21:38:47 -0500

    final

--- cpp code/itsy-bitsy-spider.cpp
+++ cpp code/itsy-bitsy-spider.cpp
@@ -7,7 +7,7 @@
 #include <tuple>
 #include <queue>
 
-int cell_number = 0; // global variable which is necessary for the visited set on the bfs
+unsigned int cell_number = 0; // global variable which is necessary for the visited set on the bfs
 
 struct Vertex {
 
@@ -22,7 +22,7 @@ struct Vertex {
     Vertex* W;
     Vertex* U;
     Vertex* D;
-    int cell; // to identify and to use on the visited set later
+    unsigned int cell; // to identify and to use on the visited set later
 
     Vertex() {
         // the default value for each pointer is null and they will be changed later as we build the edges
@@ -41,9 +41,8 @@ std::string get_directions(const std::string& binary_string){
         Given the input binary string, this function computes and returns all the directions
         that a vertex can travel to. Which will be used when setting the pointers.
     */
-
     std::string directions;
-    for (int i = 0; i < binary_string.length(); i++){
+    for (unsigned int i = 0; i < binary_string.length(); i++){
         if (binary_string[i] == '1'){
             switch(i){
                 case 0:
@@ -71,13 +70,12 @@ std::string get_directions(const std::string& binary_string){
 }
 
 // helper functions
-void write_to_tuple(std::ifstream& file, std::tuple<int, int, int>& tuple_to_add){
+void write_to_tuple(std::ifstream& file, std::tuple<unsigned int, unsigned int, unsigned int>& tuple_to_add){
 
     /*
         The first three lines are l, r, c along with the coordinates for the start and end. To avoid having repeated code
         this function takes the file that is being read from and the tuple to pass the values (from the file) to.
     */
-
     file >> std::get<0>(tuple_to_add);
     file >> std::get<1>(tuple_to_add);
     file >> std::get<2>(tuple_to_add);
@@ -92,13 +90,15 @@ void find_path_bfs_helper(Vertex current_vertex, std::vector<bool>& set_visited,
 
         Check if we've seen vertex "current_vertex" before, mark it visited, add its destination to the path and add the pair to the queue
     */
+   if (set_visited[current_vertex.cell] == true) {
+        return;
+   }
 
-    if (set_visited[current_vertex.cell] == false) { // if we have not seen the vertex yet
-        set_visited[current_vertex.cell] = true; // mark it seen
-        current_path.push_back(destination); // add the destination to current_path
-        std::pair<Vertex, std::vector<std::string>> addToQueue(current_vertex, current_path); // make the pair
-        queue.push(addToQueue); // add to queue
-    }
+    set_visited[current_vertex.cell] = true; // mark it seen
+    current_path.push_back(destination); // add the destination to current_path
+    std::pair<Vertex, std::vector<std::string>> addToQueue(current_vertex, current_path); // make the pair
+    queue.push(addToQueue); // add to queue
+
 }
 
 void write_to_file(std::vector<std::string> data){
@@ -108,13 +108,14 @@ void write_to_file(std::vector<std::string> data){
     */
 
     std::ofstream output("output.txt"); 
-    for (int i = 0; i < data.size(); i++){
+    for (unsigned int i = 0; i < data.size(); i++){
         output << data[i] << ' ';
     }
     output.close();
     std::cout << "successfully written to file" << std::endl;
 }
 
+// Graph and Bfs functions
 std::vector<std::string> find_path_bfs(Vertex start, Vertex end){
 
     /*
@@ -144,6 +145,7 @@ std::vector<std::string> find_path_bfs(Vertex start, Vertex end){
         }
 
         // adding the edges to the queue
+        // a vertex can have multiple edges/pointers hence an if-if is needed
         if (vertex.N != nullptr){
             find_path_bfs_helper(*vertex.N, visitedSet, cur_path, "N", queue);
         }
@@ -172,25 +174,22 @@ std::vector<std::string> find_path_bfs(Vertex start, Vertex end){
     return start_path; // if no path is found, returns empty path
 }
 
-std::vector<std::vector<std::vector<Vertex>>> Graph(std::tuple <int, int, int> dimensions, std::ifstream& read_file){
+std::vector<std::vector<std::vector<Vertex>>> Graph(std::tuple <unsigned int, unsigned int, unsigned int> dimensions, std::ifstream& read_file){
     /*
         Given the dimensions and file to be read from, this function will create and return a graph with vertices and pointers
         to other vertices, which corresponds to the binary string input from the file
     */
     std::vector<std::vector<std::vector<Vertex>>> graph;    // graph that will be returned
     std::vector<std::vector<std::vector<std::string>>> helper_graph; // graph that will store the read bits from the textfile
-
-    for (int z = 0; z < (std::get<0>(dimensions)*std::get<1>(dimensions)); z = z + std::get<1>(dimensions)){ // traverses until the number of (rows * levels) is reached
+    for (unsigned int z = 0; z < (std::get<0>(dimensions)*std::get<1>(dimensions)); z = z + std::get<1>(dimensions)){ // traverses until the number of (rows * levels) is reached
         // helper vectors which are used later to build upper layers of graph
         std::vector<std::vector<Vertex>> build_vertex_graph_helper; 
         std::vector<std::vector<std::string>> build_graph_level_helper; 
-
-        for (int y = 0; y < std::get<1>(dimensions); y++){ // loops until number of rows
+        for (unsigned int y = 0; y < std::get<1>(dimensions); y++){ // loops until number of rows
             // helper vectors which are used later to build the layers of the graph
             std::vector<Vertex> build_vertex_in_graph_helper; 
             std::vector<std::string> build_graph_column_helper; 
-
-            for (int x = 0; x < std::get<2>(dimensions); x++){ // loops until number of columns
+            for (unsigned int x = 0; x < std::get<2>(dimensions); x++){ // loops until number of columns
                 std::string temp_string; // temporary to hold current string read from file
                 read_file >> temp_string; 
                 build_graph_column_helper.push_back(get_directions(temp_string));
@@ -209,12 +208,11 @@ std::vector<std::vector<std::vector<Vertex>>> Graph(std::tuple <int, int, int> d
     }
 
     // creating the edges for the graph
-    for (int z = 0; z < helper_graph.size(); z++){
-        for (int y = 0; y < helper_graph[z].size(); y++){
-            for (int x = 0; x < helper_graph[z][y].size(); x++){
+    for (unsigned int z = 0; z < helper_graph.size(); z++){
+        for (unsigned int y = 0; y < helper_graph[z].size(); y++){
+            for (unsigned int x = 0; x < helper_graph[z][y].size(); x++){
                 std::string current_string = helper_graph[z][y][x]; // will go through each letter in the string and determine the dependencies (where to direct pointers)
-            
-                for (int w = 0; w < current_string.length(); w++){
+                for (unsigned int w = 0; w < current_string.length(); w++){
                     switch(current_string[w]){
                         case 'N':
                             graph[z][y][x].N = &graph[z][y-1][x]; // north is above current row
@@ -239,21 +237,20 @@ std::vector<std::vector<std::vector<Vertex>>> Graph(std::tuple <int, int, int> d
             }
         }
     }
-
     return graph;
 }
 
 int main(){
     
-    std::ifstream file("/Users/emanuelaseghehey/Development/Itsy-Bitsy-Spider-algo/textfiles/itsybitsy-maze.txt");
+    std::ifstream file("/Users/emanuelaseghehey/Development/Itsy-Bitsy-Spider-algo/textfiles/tiny-maze.txt");
 
     if (!file.is_open()){ // checking if we're able to open the file, checker to see if it exists
         throw std::runtime_error("cannot read file");
     }
 
-    std::tuple <int, int, int> size; // levels, rows, columns
-    std::tuple <int, int, int> start; // coordinates for the starting vertex
-    std::tuple <int, int, int> end; // coordinates for the destination vertex
+    std::tuple <unsigned int, unsigned int, unsigned int> size; // levels, rows, columns
+    std::tuple <unsigned int, unsigned int, unsigned int> start; // coordinates for the starting vertex
+    std::tuple <unsigned int, unsigned int, unsigned int> end; // coordinates for the destination vertex
 
     // reading and storing first three lines to their respective tuples
     write_to_tuple(file, size);
@@ -262,8 +259,9 @@ int main(){
 
     std::vector<std::vector<std::vector<Vertex>>> graph = Graph(size, file);
     file.close();
-    std::vector<std::string> path = find_path_bfs(graph[std::get<0>(start)][std::get<1>(start)][std::get<2>(start)], graph[std::get<0>(end)][std::get<1>(end)][std::get<2>(end)]);
-    write_to_file(path);
+    
+    std::vector<std::string> path_start_to_end = find_path_bfs(graph[std::get<0>(start)][std::get<1>(start)][std::get<2>(start)], graph[std::get<0>(end)][std::get<1>(end)][std::get<2>(end)]);
+    write_to_file(path_start_to_end);
 
     return 0;
 }
